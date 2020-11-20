cba863967e69b4bfe4656ff4bd0fbf5717c7caf9
radix007 <47687097+radix007@users.noreply.github.com>
2020-11-21 02:09:36 +0530

    Adding Nav Html Part in php file

---
+++ nav/nav.php
@@ -0,0 +1,46 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Nav Bar</title>
+</head>
+<body>
+    <div id="anonymous_nav_bar">
+        <!-- id="root"-->
+        <header class="">
+            <nav class="mobileMegaMenu navbarDefault">
+                <div class="largeMenu">
+                    <a class="mobileNavTrigger"><img src="nav-menu.svg"></a>
+                    <a class="anonymous_logo" href="index.html"><img src="anonymous_fiber.svg" alt="Anonymous Fiber Logo" style="" class=""></a>
+                    <div class="main Menu">
+                        <ul>
+                            <li><a href="javascript.void(0)">Fiber Cities<i class="icon-chevron-down"></i></a>
+                                <div class="submenu">
+                                    <ul>
+                                        <li><a href="Delhi.html">Delhi</a></li>
+                                        <li><a href="Gurugram.html">Gurugram</a></li>
+                                        <li><a href="Noida.html">Noida</a></li>
+                                        <li><a href="Dehradun.html">Dehradun</a></li>
+                                        <li><a href="Jaipur.html">Jaipur</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a href="Business.html">Business</a></li>
+                            <li><a href="AboutUs.html">About Us</a></li>
+                            <li><a href="ContactUs.html">Contact Us</a></li>
+                        </ul>
+
+
+                    </div>
+                    <div class="right-action">
+                        <a href="NewConnection.php" class="airtel-store">New Connection</a>
+                        <a href="Login.php" class=user-login><img src="login-user.svg">Sign In</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
+</body>
+
+</html>
