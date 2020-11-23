52e7f3d046f0e17cc7214609f05badc44575e30c
radix007 <47687097+radix007@users.noreply.github.com>
2020-11-23 20:18:04 +0530

    Added StayInTouch Php file.Also Have Linked it too

--- AboutUs.php
+++ AboutUs.php
@@ -51,21 +51,13 @@
     </section>
 
 
-    <section class="stay-in-touch">
-        <h2><br><br>We like you. Stay in touch.</h2>
-        <input type="text" placeholder="Your email address" required>
-        <input type="submit" value="Sign up">
-        <p>
-            By clicking “sign up,” you agree to receive promotional emails, which you can opt out of if you change your
-            mind. For more information, see our Privacy Notice.
+   <!--  WE LIKE YOU STAY IN TOUCH PART STARTS -->
+   <?php
+    include('stay_in_touch.php');
+    ?>
+    <!--  WE LIKE YOU STAY IN TOUCH PART ENDS -->
 
-        </p>
-        <div class="socials">
-            <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-            <a href="https://facebook.com"><i class="fab fa-facebook-square"></i></a>
-            <a href="https://github.com/radix007"><i class="fab fa-github"></i></a>
-        </div>
-    </section>
+    
     <!-- FOOTER STARTS -->
 
     <?php include('footer.php'); ?>
---
+++ stay_in_touch.php
@@ -0,0 +1,15 @@
+<section class="stay-in-touch">
+        <h2><br><br>We like you. Stay in touch.</h2>
+        <input type="text" placeholder="Your email address" required>
+        <input type="submit" value="Sign up">
+        <p>
+            By clicking “sign up,” you agree to receive promotional emails, which you can opt out of if you change your
+            mind. For more information, see our Privacy Notice.
+
+        </p>
+        <div class="socials">
+            <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+            <a href="https://facebook.com"><i class="fab fa-facebook-square"></i></a>
+            <a href="https://github.com/radix007"><i class="fab fa-github"></i></a>
+        </div>
+    </section> 
