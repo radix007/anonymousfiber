8fb075542b36e45cadefd5fe13d8d47d633c962e
radix007 <47687097+radix007@users.noreply.github.com>
2021-11-16 16:19:15 +0530

    Fixing cities css error by replacing all files

--- FiberCities.php
+++ FiberCities.php
@@ -11,9 +11,69 @@
     <meta property="og:title" content="Get  the fastest Anonymous Fiber">
     <link rel="icon" href="images/anonymous_fox.jpg" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+     <!--=============== SWIPER CSS ===============-->
+     <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+
     <title>Fiber Cities</title>
+    <style>
+        :root{
+            --img-transition: .3s;
+  --img-hidden: hidden;
+  --img-scale: scale(1.1);
+        }
+       .discover__card {
+  position: relative;
+  width: 300px;
+  overflow: var(--img-hidden);
+  color:white;
+  font-size:1.5em;
+}
+.discover {
+    width : 80%;
+    margin: auto;
+}
+.discover__data {
+  position: absolute;
+  bottom: 1.5rem;
+  left: 1rem;
+}
+
+.discover__title, 
+.discover__description {
+  color: var(--white-color);
+}
+
+.discover__title {
+  font-size: var(--h3-font-size);
+  margin-bottom: var(--mb-0-25);
+}
+
+.discover__description {
+  display: block;
+  font-size: var(--smaller-font-size);
+}
+
+.discover__img {
+  transition: var(--img-transition);
+  width:300px;
+  height:400px;
+}
+
+.discover__img:hover {
+  transform: var(--img-scale);
+}
+
+.swiper-container-3d .swiper-slide-shadow-left,
+.swiper-container-3d .swiper-slide-shadow-right {
+  background-image: none;
+}
+.swiper-container {
+	width: 100%;
+	/* height: 100%; */
+}
+    </style>
 </head>
 
 <body>
@@ -29,35 +89,57 @@
     </section>
     <section class="description" id="fibercities">
         <h1>Move ahead with lighting speed <span> Internet.</span></h1>
-        <div>
-            <div>
+</section>
+        <section class="discover section" id="discover">
+             <div class="discover__container container swiper-container">
+                 <div class="swiper-wrapper">
+                    <!--==================== DISCOVER 1 ====================-->
+                <div class="discover__card swiper-slide">
+                        <img src="images/Delhi.jpg" alt="" class="discover__img">
+                        <div class="discover__data">
+                            <h2 class="discover__title">DELHI</h2>
+                            <span class="discover__description"> <p>National Capital leading from front.<br> Over 10 million users</p></span>
+                        </div>
+                </div>
+                <!--==================== DISCOVER 2 ====================-->
+                <div class="discover__card swiper-slide">
+                        <img src="images/Mumbai.jpg" alt="" class="discover__img">
+                        <div class="discover__data">
+                            <h2 class="discover__title">MUMBAI</h2>
+                            <span class="discover__description"><p>Hai Mushkil jina yahan? Not anymore.<br> Over 5 million users</p></span>
+                        </div>
+                </div>
+                <!--==================== DISCOVER 3 ====================-->
+                <div class="discover__card swiper-slide">
+                        <img src="images/Bangalore.jpg" alt="" class="discover__img">
+                        <div class="discover__data">
+                            <h2 class="discover__title">BANGALORE</h2>
+                            <span class="discover__description"> <p>Silicon Valley Of India forging ahead at super speed.<br> Over 3 million users</p></span>
+                        </div>
+                </div>
+                <!--==================== DISCOVER 4 ====================-->
+                <div class="discover__card swiper-slide">
+                        <img src="images/Kolkata.jpg" alt="" class="discover__img">
+                        <div class="discover__data">
+                            <h2 class="discover__title">KOLKATA</h2>
+                            <span class="discover__description"> <p>Kolkata main ab sab mauj manata.<br> Over 2 million users</p></span>
+                        </div>
+                </div>
+                <!--==================== DISCOVER 5 ====================-->
+                <div class="discover__card swiper-slide">
+                        <img src="images/Chennai.jpg" alt="" class="discover__img">
+                        <div class="discover__data">
+                            <h2 class="discover__title">CHENNAI</h2>
+                            <span class="discover__description">  <p>Covering all major areas of South India.<br> Over 1 million users</p></span>
+                        </div>
+                </div>                
+             </div>
+        </section>
 
-                <img src="images/Delhi.jpg" alt="">
-                <h1>DELHI</h1><br>
-                <p>National Capital leading from front.<br> Over 10 million users</p>
-            </div>
-            <div>
-                <img src="images/Mumbai.jpg" alt="">
-                <h1>MUMBAI</h1><br>
-                <p>Hai Mushkil jina yahan? Not anymore.<br> Over 5 million users</p>
-            </div>
-            <div>
-                <img src="images/Bangalore.jpg" alt="">
-                <h1>BANGALORE</h1><br>
-                <p>Silicon Valley Of India forging ahead at super speed.<br> Over 3 million users</p>
-            </div>
-            <div>
-                <img src="images/Kolkata.jpg" alt="">
-                <h1>KOLKATA</h1><br>
-                <p>Kolkata main ab sab mauj manata.<br> Over 2 million users</p>
-            </div>
-            <div>
-                <img src="images/Chennai.jpg" alt="">
-                <h1>CHENNAI</h1><br>
-                <p>Covering all major areas of South India.<br> Over 1 million users</p>
-            </div>
-        </div>
-    </section>
+    
+ 
+
+       
 
    <!--  WE LIKE YOU STAY IN TOUCH PART STARTS -->
    <?php
@@ -71,6 +153,11 @@
     <?php include('footer.php'); ?>
 
     <!-- FOOTER ENDS -->
+       <!--=============== SWIPER JS ===============-->
+       <script src="assets/js/swiper-bundle.min.js"></script>
+        <!--=============== MAIN JS ===============-->
+    <script src="assets/js/main.js"></script>
+
 
 </body>
 
