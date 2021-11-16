52961d01061c4d94d5fc91a8cace5ef7e54b4c3c
radix007 <47687097+radix007@users.noreply.github.com>
2021-11-16 16:14:36 +0530

    Making changes to fiber cities page

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
+
+
+
+$(document).ready(function () {
+    initParadoxWay();
+});
